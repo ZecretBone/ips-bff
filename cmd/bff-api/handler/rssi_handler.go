ae14415886df0f66078d599be10f732728c2b971
naphatcmkl <85152768+naphatcmkl@users.noreply.github.com>
2024-01-03 19:09:15 +0700

    use rest to insert data todo is check for device info header

--- cmd/bff-api/handler/rssi_handler.go
+++ cmd/bff-api/handler/rssi_handler.go
@@ -1,13 +1,14 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	v1 "github.com/ZecretBone/ips-bff/internal/gen/proto/ips/rssi/v1"
 	rssiv1 "github.com/ZecretBone/ips-bff/internal/gen/proto/ips/shared/rssi/v1"
 	rssiclient "github.com/ZecretBone/ips-bff/internal/repository/RSSIClient"
 	"github.com/gin-gonic/gin"
-	"google.golang.org/protobuf/types/known/timestamppb"
+	//"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type RSSIHandler interface {
@@ -25,38 +26,52 @@ func ProvideRSSIHandler(rssiClient rssiclient.Service) RSSIHandler {
 }
 
 func (rs *rssiHandler) Get(ctx *gin.Context) {
-	body := v1.CollectDataRequest{
-		Signals: []*rssiv1.RSSI{
-			{
-				Ssid:        "newtONE",
-				MacAddress:  "ab:cd:ef:gh:xy:xz",
-				Strength:    60.3,
-				PollingRate: 300,
-				CreatedAt:   timestamppb.Now(),
-			}, {
-				Ssid:        "newtTWO",
-				MacAddress:  "ab:cd:ef:gh:xy:x1",
-				Strength:    50.3,
-				PollingRate: 300,
-				CreatedAt:   timestamppb.Now(),
-			},
-		},
-		DeviceInfo: &rssiv1.DeviceInfo{
-			DeviceId: "FXM1234",
-			Models:   "androidOreo123",
-		},
-		Stage: rssiv1.StatCollectionStage_STAT_COLLECTION_STAGE_MULTIPLE,
-		Position: &rssiv1.Position{
-			X: 3,
-			Y: 2,
-			Z: 1,
-		},
-		Duration:  10,
-		StartedAt: timestamppb.Now(),
-		EndedAt:   timestamppb.Now(),
-		CreatedAt: timestamppb.Now(),
+	var body v1.CollectDataRequest
+	if err := ctx.BindJSON(&body); err != nil {
+		//err
+		fmt.Println("theres error in binding json")
 	}
+	body.Stage = rssiv1.StatCollectionStage_STAT_COLLECTION_STAGE_MULTIPLE
+	a := ctx.GetHeader("DeviceId")
+	b := ctx.GetHeader("Models")
+	c := ctx.GetHeader("Goose")
+	fmt.Println(c)
 
+	body.DeviceInfo = &rssiv1.DeviceInfo{
+		DeviceId: a,
+		Models:   b,
+	}
+	// body := v1.CollectDataRequest{
+	// 	Signals: []*rssiv1.RSSI{
+	// 		{
+	// 			Ssid:        "newtONE",
+	// 			MacAddress:  "ab:cd:ef:gh:xy:xz",
+	// 			Strength:    60.3,
+	// 			PollingRate: 300,
+	// 			CreatedAt:   timestamppb.Now(),
+	// 		}, {
+	// 			Ssid:        "newtTWO",
+	// 			MacAddress:  "ab:cd:ef:gh:xy:x1",
+	// 			Strength:    50.3,
+	// 			PollingRate: 300,
+	// 			CreatedAt:   timestamppb.Now(),
+	// 		},
+	// 	},
+	// 	DeviceInfo: &rssiv1.DeviceInfo{
+	// 		DeviceId: "FXM1234",
+	// 		Models:   "androidOreo123",
+	// 	},
+	// 	Stage: rssiv1.StatCollectionStage_STAT_COLLECTION_STAGE_MULTIPLE,
+	// 	Position: &rssiv1.Position{
+	// 		X: 3,
+	// 		Y: 2,
+	// 		Z: 1,
+	// 	},
+	// 	Duration:  10,
+	// 	StartedAt: timestamppb.Now(),
+	// 	EndedAt:   timestamppb.Now(),
+	// 	CreatedAt: timestamppb.Now(),
+	// }
 	if _, err := rs.rssiClient.CollectData(ctx, &body); err != nil {
 		ctx.JSON(http.StatusBadRequest, err.Error())
 	}
